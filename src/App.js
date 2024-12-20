0ceb50e8aab2c281edcd0d9e1a0c372e13c10272
Harriet2004 <s3933558@student.rmit.edu.au>
2024-12-20 15:49:22 +0400

    Added location and date boxes

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ const api = {
 }
 
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+    let date = d.getDate();
+
+    return `${day} ${date} ${month} ${year}`
+  }
   return (
     <div className="App">
       <main>
         <div className = "search-box">
           <input type = "text" className = "search-bar" placeholder = "Hello" />
         </div>
+        <div className = "location-box"> Dubai, UAE </div>
+        <div className = "date"> {dateBuilder(new Date())} </div>
       </main>
     </div>
   );
